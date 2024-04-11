1844e6424b51567a2387c0d481a615681b7219c7
KarlaJulyana <karla@solarz.com.br>
2024-04-11 09:57:47 -0300

    refactor: ident request functions

--- src/services/bookings/getBookingById.ts
+++ src/services/bookings/getBookingById.ts
@@ -2,16 +2,16 @@ import axios, { AxiosResponse } from 'axios';
 import { Booking } from '../../@types/Booking';
 
 export async function getBookingById(baseURL: string, id: number): Promise<Booking> {
-    try {
-      const response: AxiosResponse<Booking> = await axios.get(
-        `${baseURL}/bookings/${id}`
-      );
-      return response.data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        throw new Error(`Failed to get booking by ID: ${(error as Error).message}`);
-      } else {
-        throw new Error(`Failed to get booking by ID: ${String(error)}`);
-      }
+  try {
+    const response: AxiosResponse<Booking> = await axios.get(
+      `${baseURL}/bookings/${id}`
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Failed to get booking by ID: ${(error as Error).message}`);
+    } else {
+      throw new Error(`Failed to get booking by ID: ${String(error)}`);
     }
+  }
 }
--- src/services/bookings/listBookings.ts
+++ src/services/bookings/listBookings.ts
@@ -2,16 +2,16 @@ import axios, { AxiosResponse } from 'axios';
 import { Booking } from '../../@types/Booking';
 
 export async function listBookings(baseURL: string): Promise<Booking[]> {
-    try {
-      const response: AxiosResponse<Booking[]> = await axios.get(
-        `${baseURL}/bookings`
-      );
-      return response.data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        throw new Error(`Failed to list bookings: ${(error as Error).message}`);
-      } else {
-        throw new Error(`Failed to list bookings: ${String(error)}`);
-      }
+  try {
+    const response: AxiosResponse<Booking[]> = await axios.get(
+      `${baseURL}/bookings`
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Failed to list bookings: ${(error as Error).message}`);
+    } else {
+      throw new Error(`Failed to list bookings: ${String(error)}`);
     }
+  }
 }
